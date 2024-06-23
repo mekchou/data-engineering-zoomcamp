ab799085836527ffb4f6965cf205e0a64030b003
mek <mek.chou@gmail.com>
2024-06-23 11:18:07 -0400

    update files for remote backend:

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -1,41 +1,50 @@
-variable "credentials" {
-  description = "My Credentials"
-  default     = "<Path to your Service Account json file>"
-  #ex: if you have a directory where this file is called keys with your service account json file
-  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+## infrastructure variables
+
+variable "s3_bucket_name_datalake" {
+  description = "Name for the S3 bucket used for Zoomcamp Datalake"
+  type        = string
+  default     = "datalake-bucket-zoomcamp-2023"
 }
 
-
-variable "project" {
-  description = "Project"
-  default     = "<Your Project ID>"
+variable "redshift_cluster_name_dataset" {
+  description = "Name for the Redshift cluster"
+  type = string
+  default = "zoomcamp-dataset-redshift"
 }
 
-variable "region" {
-  description = "Region"
-  #Update the below to your desired region
-  default     = "us-central1"
+variable "redshift_cluster_db_dataset" {
+  description = "The database name for Redshift cluster for dataset"
+  type = string
+  default = "trips_data_all"
 }
 
-variable "location" {
-  description = "Project Location"
-  #Update the below to your desired location
-  default     = "US"
+variable "redshift_iam_roles" {
+  description = "A list of IAM ARNs that should be associated with the provisioned Redshift service"
+  type = list
+  default = ["arn:aws:iam::571772404385:role/redshift-service-role"]
 }
 
-variable "bq_dataset_name" {
-  description = "My BigQuery Dataset Name"
-  #Update the below to what you want your dataset to be called
-  default     = "demo_dataset"
+variable "redshift_service_role" {
+  description = "IAM role to be attached to provisioned Redshift service. Note that this role should also be included in the redshift_iam_roles list."
+  type = string
+  default = "arn:aws:iam::571772404385:role/redshift-service-role"
 }
 
-variable "gcs_bucket_name" {
-  description = "My Storage Bucket Name"
-  #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+variable "redshift_security_group_list" {
+  description = "The list of security group IDs to be associated with the provisioned Redshift service"
+  type = list
+  default = ["sg-0cf5b9ef80fe2fca1"]
 }
 
-variable "gcs_storage_class" {
-  description = "Bucket Storage Class"
-  default     = "STANDARD"
+variable "redshift_serverless_ns_name" {
+  description = "The name of Redshift Serverless namespace"
+  type = string
+  default = "zoomcamp-redshift"
+}
+
+variable "redshift_serverless_wg_name" {
+  description = "The name of Redshift Serverless workgroup"
+  type = string
+  default = "zoomcamp-redshift-workgroup"
+  
 }
