a298502f9788724bd30613777fe61207a08a35a8
mek <mek.chou@gmail.com>
2024-06-23 11:43:12 -0400

    change var to match current set up

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -3,7 +3,7 @@
 variable "s3_bucket_name_datalake" {
   description = "Name for the S3 bucket used for Zoomcamp Datalake"
   type        = string
-  default     = "datalake-bucket-zoomcamp-2023"
+  default     = "datalake-bucket-zoomcamp-2024"
 }
 
 variable "redshift_cluster_name_dataset" {
@@ -21,19 +21,19 @@ variable "redshift_cluster_db_dataset" {
 variable "redshift_iam_roles" {
   description = "A list of IAM ARNs that should be associated with the provisioned Redshift service"
   type = list
-  default = ["arn:aws:iam::571772404385:role/redshift-service-role"]
+  default = ["arn:aws:iam::381492227382:role/redshift-service-role"]
 }
 
 variable "redshift_service_role" {
   description = "IAM role to be attached to provisioned Redshift service. Note that this role should also be included in the redshift_iam_roles list."
   type = string
-  default = "arn:aws:iam::571772404385:role/redshift-service-role"
+  default = "arn:aws:iam::381492227382:role/redshift-service-role"
 }
 
 variable "redshift_security_group_list" {
   description = "The list of security group IDs to be associated with the provisioned Redshift service"
   type = list
-  default = ["sg-0cf5b9ef80fe2fca1"]
+  default = ["sg-079017e17adf32cd0"]
 }
 
 variable "redshift_serverless_ns_name" {
