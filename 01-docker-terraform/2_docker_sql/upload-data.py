d870e3ce0c3158ffafc8eca4d9ae82f11f231c94
Ubuntu <ubuntu@ip-172-31-45-248.us-east-2.compute.internal>
2024-06-23 00:41:20 +0000

    only load top 100 rows

--- 01-docker-terraform/2_docker_sql/upload-data.py
+++ 01-docker-terraform/2_docker_sql/upload-data.py
@@ -25,7 +25,7 @@ def main(params):
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
     df.head(n=0).to_sql(name=table, con=engine, if_exists='replace')
-    df.to_sql(name=table, con=engine, if_exists='append')
+    df.head(100).to_sql(name=table, con=engine, if_exists='append')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
