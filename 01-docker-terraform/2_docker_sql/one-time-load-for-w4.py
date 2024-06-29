16cc12583fb8ad1c73dd8a681987e871797a98b0
mek <mek.chou@gmail.com>
2024-06-29 15:52:28 -0400

    load taxi zone lookup

--- 01-docker-terraform/2_docker_sql/one-time-load-for-w4.py
+++ 01-docker-terraform/2_docker_sql/one-time-load-for-w4.py
@@ -14,10 +14,10 @@ def main():
     host = 'localhost'
     port = 5432
     db = 'ny_taxi'
-    table = 'fhv_taxi'
+    table = 'taxi_zone_lookup'
     # url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_'
-    url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/fhv_tripdata_'
-    file_format = '.csv.gz'
+    url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/'
+    file_format = '.csv'
     # taxi_dtypes = {
     #                 'VendorID': pd.Int64Dtype(),
     #                 'passenger_count': pd.Int64Dtype(),
@@ -38,39 +38,40 @@ def main():
     #                 'trip_type':pd.Int64Dtype(),
     #                 'congestion_surcharge':float
     #             }
-    taxi_dtypes = {
-                    'dispatching_base_num':str,
-                    'PULocationID':pd.Int64Dtype(),
-                    'DOLocationID':pd.Int64Dtype(),
-                    'SR_Flag': pd.Int64Dtype(),
-                    'Affiliated_base_number':str
-                }
+    # taxi_dtypes = {
+    #                 'dispatching_base_num':str,
+    #                 'PULocationID':pd.Int64Dtype(),
+    #                 'DOLocationID':pd.Int64Dtype(),
+    #                 'SR_Flag': pd.Int64Dtype(),
+    #                 'Affiliated_base_number':str
+    #             }
     engine = create_engine(f'postgresql://{user}:{pw}@{host}:{port}/{db}')
-    new_column_names = {
-        'PUlocationID': 'PULocationID',
-        'DOlocationID': 'DOLocationID',
+    # new_column_names = {
+        # 'PUlocationID': 'PULocationID',
+        # 'DOlocationID': 'DOLocationID',
         # Add more mappings as needed
-    }
+    # }
     # parse_dates = ['lpep_pickup_datetime', 'lpep_dropoff_datetime']
     # parse_dates = ['pickup_datetime', 'dropoff_datetime']
     # file_list = ['2020-03', '2020-04', '2020-05', '2020-06', '2020-07', '2020-08', '2020-09', '2020-10', '2020-11', '2020-12', '2021-01', '2021-02', '2021-03', '2021-04', '2021-05', '2021-06', '2021-07']
     # file_list = ['2019-02', '2019-03', '2019-04', '2019-05', '2019-06', '2019-07', '2019-08', '2019-09', '2019-10', '2019-11', '2019-12', '2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07', '2020-08', '2020-09', '2020-10', '2020-11', '2020-12', '2021-01', '2021-02', '2021-03', '2021-04', '2021-05', '2021-06', '2021-07']
     # file_list = ['2019-01', '2019-02', '2019-03', '2019-04', '2019-05', '2019-06', '2019-07', '2019-08', '2019-09', '2019-10', '2019-11', '2019-12', '2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07', '2020-08', '2020-09', '2020-10', '2020-11', '2020-12', '2021-01', '2021-02', '2021-03', '2021-04', '2021-05', '2021-06', '2021-07']
-    file_list = ['2019-01']
+    file_list = ['taxi_zone_lookup']
     t_start = time()
     for file in file_list:
         print(f'reading {file}...')
         r_start = time()
         # df = pd.read_csv(filepath_or_buffer=f'{url}{file}{file_format}',sep=',',compression='gzip',dtype=taxi_dtypes,parse_dates=parse_dates)
         # df = pd.read_csv(filepath_or_buffer=f'{url}{file}{file_format}',sep=',',compression='gzip',dtype=taxi_dtypes)
-        df_iter = pd.read_csv(filepath_or_buffer=f'{url}{file}{file_format}',sep=',',compression='gzip',dtype=taxi_dtypes, iterator=True,chunksize=100000)
+        # df_iter = pd.read_csv(filepath_or_buffer=f'{url}{file}{file_format}',sep=',',compression='gzip',dtype=taxi_dtypes, iterator=True,chunksize=100000)
+        df_iter = pd.read_csv(filepath_or_buffer=f'{url}{file}{file_format}',sep=',', iterator=True,chunksize=100000)
         # print(df.columns)
         r_end = time()
         print(f'read! time taken {r_end-r_start:10.3f} seconds.\n')
         # for batch in df:
         for df in df_iter:
         # print(df.columns)
-            df = df.rename(columns = new_column_names)            
+            # df = df.rename(columns = new_column_names)            
             print(f'inserting {file}...')
             w_start = time()
             df.to_sql(name=table, con=engine, if_exists='append', index=False)
