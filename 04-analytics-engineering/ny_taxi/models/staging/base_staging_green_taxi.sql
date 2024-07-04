293a4c65f5a2ca0d8fda34aa3098fae82c30a6c2
mek <mek.chou@gmail.com>
2024-07-04 14:36:26 -0400

    add transformation and cleanup

--- 04-analytics-engineering/ny_taxi/models/staging/base_staging_green_taxi.sql
+++ 04-analytics-engineering/ny_taxi/models/staging/base_staging_green_taxi.sql
@@ -1,31 +1,41 @@
-with source as (
-      select * from {{ source('staging', 'green_taxi') }}
-),
-renamed as (
-    select
-        {{ dbt_utils.generate_surrogate_key(['"VendorID"', 'lpep_pickup_datetime']) }} as tripid
-		,"VendorID"
-	    , lpep_pickup_datetime
-	    , lpep_dropoff_datetime
-	    , store_and_fwd_flag
-	    , "RatecodeID"
-	    , "PULocationID"
-	    , "DOLocationID"
-	    , passenger_count
-	    , trip_distance
-	    , fare_amount
-	    , extra
-	    , mta_tax
-	    , tip_amount
-	    , tolls_amount
-	    , ehail_fee
-	    , improvement_surcharge
-	    , total_amount
-	    , payment_type
-	    , trip_type
-	    , congestion_surcharge
-
-    from source
+with tripdata as 
+(
+    select *
+		, row_number() over(partition by "VendorID", lpep_dropoff_datetime) as rn
+	from {{ source('staging', 'green_taxi') }}
 )
-select * from renamed
-  
+
+select
+	-- identifiers
+    {{ dbt_utils.generate_surrogate_key(['"VendorID"', 'lpep_pickup_datetime']) }} as tripid
+	, "VendorID" as vendorid
+	, "RatecodeID" as ratecodeid
+	, "PULocationID" as pickup_locationid
+	, "DOLocationID" as dropoff_locationid
+	-- timestamps
+	, lpep_pickup_datetime as pickup_datetime
+	, lpep_dropoff_datetime as dropoff_datetime
+	-- trip_info
+	, store_and_fwd_flag
+	, passenger_count
+	, trip_distance
+	, trip_type
+	-- payment info
+	, fare_amount
+	, extra
+	, mta_tax
+	, tip_amount
+	, tolls_amount
+	, ehail_fee
+	, improvement_surcharge
+	, congestion_surcharge
+	, total_amount
+	, payment_type
+	, {{get_payment_type_description("payment_type")}} as payment_type_description
+from tripdata
+where
+	rn = 1
+
+{% if var('is_test_run', default=true) %}
+limit 100
+{% endif %}
--- 04-analytics-engineering/ny_taxi/models/staging/base_staging_yellow_taxi.sql
+++ 04-analytics-engineering/ny_taxi/models/staging/base_staging_yellow_taxi.sql
@@ -1,28 +1,42 @@
-with source as (
-      select * from {{ source('staging', 'yellow_taxi') }}
-),
-renamed as (
-    select
-    	"VendorID"
-	    , tpep_pickup_datetime
-	    , tpep_dropoff_datetime
-	    , passenger_count
-	    , trip_distance
-	    , "RatecodeID"
-	    , store_and_fwd_flag
-	    , "PULocationID"
-	    , "DOLocationID"
-	    , {{get_payment_type_description("payment_type")}} as payment_type_descripted
-	    , fare_amount
-	    , extra
-	    , mta_tax
-	    , tip_amount
-	    , tolls_amount
-	    , improvement_surcharge
-	    , total_amount
-	    , congestion_surcharge
-
-    from source
+with tripdata as 
+(
+    select * 
+		, row_number() over(partition by "VendorID", tpep_dropoff_datetime) as rn
+	from {{ source('staging', 'yellow_taxi') }}
 )
-select * from renamed
-  
+
+select
+	-- identifiers
+	{{ dbt_utils.generate_surrogate_key(['"VendorID"', 'tpep_pickup_datetime']) }} as tripid
+	, "VendorID" as vendorid
+    , "RatecodeID" as ratecodeid
+    , "PULocationID" as pickup_locationid
+    , "DOLocationID" as dropoff_locationid
+	-- timestamps
+    , tpep_pickup_datetime as pickup_datetime
+    , tpep_dropoff_datetime as dropoff_datetime
+    -- trip_info
+	, store_and_fwd_flag
+    , passenger_count
+    , trip_distance
+    -- yellow cabs are always street-hail
+	, 1 as trip_type
+	-- payment info
+    , fare_amount
+    , extra
+    , mta_tax
+    , tip_amount
+    , tolls_amount
+	, 0 as ehail_fee
+    , improvement_surcharge
+    , congestion_surcharge
+    , total_amount
+	, payment_type
+	, {{get_payment_type_description("payment_type")}} as payment_type_description
+from tripdata
+where
+	rn = 1
+
+{% if var('is_test_run', default=true) %}
+limit 100
+{% endif %}
