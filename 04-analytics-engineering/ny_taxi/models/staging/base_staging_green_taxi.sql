d19b682680f54c492715d19c1fe8aa8db38567a5
mek <mek.chou@gmail.com>
2024-07-04 10:57:09 -0400

    build successfully

--- 04-analytics-engineering/ny_taxi/models/staging/base_staging_green_taxi.sql
+++ 04-analytics-engineering/ny_taxi/models/staging/base_staging_green_taxi.sql
@@ -3,7 +3,26 @@ with source as (
 ),
 renamed as (
     select
-        
+        "VendorID"
+	    , lpep_pickup_datetime
+	    , lpep_dropoff_datetime
+	    , store_and_fwd_flag
+	    , "RatecodeID"
+	    , "PULocationID"
+	    , "DOLocationID"
+	    , passenger_count
+	    , trip_distance
+	    , fare_amount
+	    , extra
+	    , mta_tax
+	    , tip_amount
+	    , tolls_amount
+	    , ehail_fee
+	    , improvement_surcharge
+	    , total_amount
+	    , payment_type
+	    , trip_type
+	    , congestion_surcharge
 
     from source
 )
--- 04-analytics-engineering/ny_taxi/models/staging/base_staging_yellow_taxi.sql
+++ 04-analytics-engineering/ny_taxi/models/staging/base_staging_yellow_taxi.sql
@@ -3,7 +3,24 @@ with source as (
 ),
 renamed as (
     select
-        
+    	"VendorID"
+	    , tpep_pickup_datetime
+	    , tpep_dropoff_datetime
+	    , passenger_count
+	    , trip_distance
+	    , "RatecodeID"
+	    , store_and_fwd_flag
+	    , "PULocationID"
+	    , "DOLocationID"
+	    , {{get_payment_type_description("payment_type")}} as payment_type_descripted
+	    , fare_amount
+	    , extra
+	    , mta_tax
+	    , tip_amount
+	    , tolls_amount
+	    , improvement_surcharge
+	    , total_amount
+	    , congestion_surcharge
 
     from source
 )
